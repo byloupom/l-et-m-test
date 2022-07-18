4db60e3413e95cd85babb56166783d7e6ee95be3
Loupom <109140212+byloupom@users.noreply.github.com>
2022-07-18 12:11:14 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,35 +3,49 @@ import "./App.css";
 import { ThemeProvider, CssBaseline, Container, Box } from "@mui/material";
 import Hero from "./components/Hero";
 
-import { Routes, Route, Link, Redirect} from "react-router-dom";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import Accueil from "./components/Accueil/Accueil";
 import Dormir from "./components/Dormir/Dormir";
 import betise from "./images/betise.png";
+import { useLayoutEffect } from 'react'
 
-
+const Wrapper = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    // document.documentElement.scrollTo({top: 0, behavior: "smooth"});
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+} 
 
 function App() {
-
-
-
   return (
     <div className="App">
-      <ThemeProvider theme={customTheme}>
-        <CssBaseline enableColorScheme />
-        <Link to="/l-et-m" style={{ textDecoration: "none" }}>
-          <Hero />
-        </Link>
-        <Container maxWidth="md">
-          <Routes>
-            <Route path="/" element={<Accueil />}/>
-            <Route path="/dormir" element={<Dormir />} />
-            <Route path="*" element={<h1>Page not found.</h1>} />
-          </Routes>
-          <Box display="flex" alignItems="center" justifyContent="center" mt={6} mb={6} sx={{transform: "rotate(13deg)"}}>
-            <img src={betise} alt="Bétise de Cambray" width="100px" />
-          </Box>
-        </Container>
-      </ThemeProvider>
+      <Wrapper>
+        <ThemeProvider theme={customTheme}>
+          <CssBaseline enableColorScheme />
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Hero />
+          </Link>
+          <Container maxWidth="md">
+            <Routes>
+              <Route path="/" element={<Accueil />} />
+              <Route path="/dormir" element={<Dormir />} />
+              <Route path="*" element={<h1>Page not found.</h1>} />
+            </Routes>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              mt={6}
+              mb={6}
+              sx={{ transform: "rotate(13deg)" }}
+            >
+              <img src={betise} alt="Bétise de Cambray" width="100px" />
+            </Box>
+          </Container>
+        </ThemeProvider>
+      </Wrapper>
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,23 @@ import { Container } from "@mui/system";
 import betise from "../images/betise.png";
 const Hero = () => {
   return (
-    <Container maxWidth="lg">
-      <Grid container spacing={4} mt={6} mb={8}>
+    <Container maxWidth="lg" >
+      <Box display="flex" 
+        alignItems="center"
+        justifyContent="center"><Typography 
+        color="text.secondary"
+        sx={{ mt: 1, fontFamily: "Monospace" }}
+
+      >
+        Mariage de Marianne et Louis
+      </Typography></Box>
+      <Grid container spacing={4} mt={1} mb={8}>
         <Grid item xs={12} sx={{ textAlign: "center" }}>
           <Box>
-              <img src={betise} alt="Bétise de Cambray" />
+            <img src={betise} alt="Bétise de Cambray" />
             <Typography
               variant="h3"
-              color= "text.primary"
+              color="text.primary"
               sx={{
                 fontFamily: "Berkshire Swash",
                 fontWeight: "bold",
@@ -20,8 +29,11 @@ const Hero = () => {
               Les Bétises de Cambray
             </Typography>
           </Box>
-          <Typography color="text.secondary" sx={{ mt: 1, fontFamily: "Monospace" }}>
-            Mariage de Marianne et Louis - 10 Sept. 2022
+          <Typography
+            color="text.secondary"
+            sx={{ mt: 1, fontFamily: "Monospace" }}
+          >
+            Château de Cambray - 10 Sept. 2022
           </Typography>
         </Grid>
       </Grid>
